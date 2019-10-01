4dd634b5df8488c67db3acefc4b05e431ae6cafd
jessimac <jessica.a.montoya@gmail.com>
2019-10-01 16:11:25 -0700

    producer updates, readme updates

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-foliodream
+# FolioDream
+
+Using real-time and historical stock market price data, I’ve developed an app that will help investors pick a low-risk portfolio.
+
+![Pipeline Diagram](images/pipeline.png)
+
+
--- src/kafka/stockdata_producer.py
+++ src/kafka/stockdata_producer.py
@@ -1,21 +1,45 @@
-############################################################
-# Kafka producer which 
-# reads data from S3 bucket
-# and forwards to broker topic. 
-############################################################
+###########################################################
+# Kafka producer 
+# * Reads data from S3 bucket
+# * Reformats data into json 
+# * Forwards to broker topic. 
+##########################################################
 
 from kafka import KafkaProducer
+from datetime import datetime
 import time
 import boto3
 import botocore
+from IEXTools import IEXAPI
+from IEXTools import Parser, messages
+import sys
+from producer_keys import KAFKA_IP, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 def main():
 
-        producer = KafkaProducer(bootstrap_servers = 'KAFKA_IP:9092')
 
-        s3 = boto3.resource('s3', aws_access_key_id = 'AWS_ACCESS_KEY_ID', aws_secret_access_key = 'AWS_SECRET_ACCESS_KEY')
-        bucket = s3.Bucket('historical-stock-data-insightds')
+        # get producer
+        producer = KafkaProducer(bootstrap_servers = (KAFKA_IP) + ':9092', api_version=(1,0,0))
 
+        # get bucket
+        # s3 = boto3.resource('s3', aws_access_key_id = AWS_ACCESS_KEY_ID, aws_secret_access_key = AWS_SECRET_ACCESS_KEY)
+        s3 = boto3.resource('s3')
+        #bucket = s3.Bucket('historical-stock-data-insightds')
+
+        # get list of historical trading days
+        tradingdays = open("../historical_data/tradingdays.txt","r") 
+
+        #with open("../../historical_data/tradingdays.txt") as tradingdays:
+        for day in tradingdays: 
+               #line = fp.readline()
+               s3_object = s3.Object('historical-stock-data-insightds',day.rstrip() +'.pcap')
+               #historical-stock-data-insightds
+               pcappath = 'https://s3-us-west-2.amazonaws.com/historical-stock-data-insightds/' + day.rstrip() + '.pcap'
+
+               iex_parser = Parser(pcappath)
+               print(day)
+
+        '''
         #t bucket contains a list of csv links that point to data for each hour of trading
         # access each pcap file
         for object in bucket.objects.all():
@@ -28,6 +52,7 @@ def main():
                         output = output + str(element) + "^"
                         producer.send('stock-update', output.encode())
                         producer.flush()
+        '''
 
         return
 
